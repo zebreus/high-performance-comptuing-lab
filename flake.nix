7fc2b7752fe9513b85c5b5e66d0a8ceb5944f85a
Zebreus <lennarteichhorn@googlemail.com>
2023-11-01 17:32:31 +0100

    Add asciidoc framework

--- flake.nix
+++ flake.nix
@@ -3,13 +3,18 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=refs/heads/master";
+    nixpkgs-asciidoc.url = "github:zebreus/nixpkgs?ref=f1a3be7a1160cc4810c0274ab76f0fac813eb4d6";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, nixpkgs-asciidoc }:
     flake-utils.lib.eachDefaultSystem (system:
       with nixpkgs.legacyPackages.${system};
+      let
+        pkgs-with-asciidoc = import nixpkgs-asciidoc { inherit system; };
+      in
       rec {
+
         name = "high-performance-computing";
         packages.default =
           llvmPackages_16.stdenv.mkDerivation {
@@ -25,6 +30,15 @@
               clang-tools_16
               lldb
               nil
+
+              pkgs-with-asciidoc.python3
+              pkgs-with-asciidoc.asciidoctor-web-pdf
+              pkgs-with-asciidoc.asciidoctor-js
+              pkgs-with-asciidoc.sass
+              pkgs-with-asciidoc.gnumake
+              pkgs-with-asciidoc.nixpkgs-fmt
+              pkgs-with-asciidoc.nil
+              pkgs-with-asciidoc.jq
             ];
           };
       }

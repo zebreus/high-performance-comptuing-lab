b04a4509244236c16297987a700929789817aa84
Zebreus <lennarteichhorn@googlemail.com>
2023-11-15 00:19:24 +0100

    Add script to benchmark on virgo cluster

---
+++ MatrixMult/virgoBenchmark.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+
+INITIAL_DIR=$(pwd)
+
+if test -z "$1"; then
+    echo "No output file specified."
+    exit 1
+fi
+
+filename=$(readlink -f $1)
+
+if test -e "$filename"; then
+    echo "Output file $filename already exists. Please remove it before continuing."
+    exit 1
+fi
+
+if ! test -e "./compareRayonAndOpenmp.sh"; then
+    echo "./matrix_openmp is missing or not executable"
+    exit 1
+fi
+
+if ! test -x "./matrix_openmp"; then
+    echo "./matrix_openmp is missing or not executable"
+    exit 1
+fi
+
+if ! test -x "./matrix_rayon"; then
+    echo "./matrix_rayon is missing or not executable"
+    exit 1
+fi
+
+if ! test -x "./matrix_generator"; then
+    echo "./matrix_generator is missing or not executable"
+    exit 1
+fi
+
+if test -z "$LUSTRE_HOME"; then
+    echo "LUSTRE_HOME is not set"
+    exit 1
+fi
+
+WORK_DIR=$(mktemp -d --tmpdir=$LUSTRE_HOME)
+mkdir -p $WORK_DIR
+cd $WORK_DIR
+
+cp $INITIAL_DIR/matrix_openmp .
+cp $INITIAL_DIR/matrix_rayon .
+cp $INITIAL_DIR/matrix_generator .
+cp $INITIAL_DIR/compareRayonAndOpenmp.sh .
+chmod a+x ./compareRayonAndOpenmp.sh
+output_filename=$WORK_DIR/results.csv
+
+RUNS=20
+
+echo "name,threads,matrix_size,duration" >$output_filename
+
+for NUM_THREADS in 2 4 8 16 32 64 128; do
+    TEST_RUN=1
+    while [ $TEST_RUN -le $RUNS ]; do
+        JOBNAME="matrix-mult-$NUM_THREADS-$TEST_RUN"
+
+        sbatch --partition=main --job-name=$JOBNAME --cpus-per-task=$NUM_THREADS --ntasks=1 --nodes=1 --output %j.out -- ./compareRayonAndOpenmp.sh $((NUM_THREADS / 2)) $output_filename
+        # bash ./compareRayonAndOpenmp.sh $NUM_THREADS $filename
+
+        ((TEST_RUN++))
+    done
+done
+
+cp $output_filename $filename
+rm -rf $WORK_DIR

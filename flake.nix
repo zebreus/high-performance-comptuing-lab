cb27d579c2406fbc0f2c22cc988de7632f35f25c
Zebreus <lennarteichhorn@googlemail.com>
2023-11-14 22:37:57 +0100

    Add a implementation with rayon

--- flake.nix
+++ flake.nix
@@ -2,16 +2,21 @@
   description = "High-performance computing projects";
 
   inputs = {
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nixpkgs.url = "github:nixos/nixpkgs?ref=refs/heads/master";
     nixpkgs-asciidoc.url = "github:zebreus/nixpkgs?ref=f1a3be7a1160cc4810c0274ab76f0fac813eb4d6";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, nixpkgs-asciidoc }:
+  outputs = { self, nixpkgs, flake-utils, nixpkgs-asciidoc, fenix }:
     flake-utils.lib.eachDefaultSystem (system:
       with nixpkgs.legacyPackages.${system};
       let
         pkgs-with-asciidoc = import nixpkgs-asciidoc { inherit system; };
+        pkgs-fenix = fenix.packages.${system};
       in
       rec {
 
@@ -34,6 +39,15 @@
               clang-tools_16
               lldb
               nil
+
+              (pkgs-fenix.complete.withComponents [
+                "cargo"
+                "clippy"
+                "rust-src"
+                "rustc"
+                "rustfmt"
+              ])
+              pkgs-fenix.rust-analyzer
 
               pkgs-with-asciidoc.python3
               pkgs-with-asciidoc.deno
